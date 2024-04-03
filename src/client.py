e2dbd00e22a7dd67a8920ac5212071c72c24f438
shreyabirthare <shreyabirthare16@gmail.com>
2024-04-03 02:12:22 -0400

    Lab2: Update code for latency calculation
    Updated client.py for latency calculation and some cosmetic changes

--- src/client.py
+++ src/client.py
@@ -14,7 +14,8 @@ client_no=random.randint(1,1000)
 def perform_session():
     # Open an HTTP connection with the front-end service
     session = requests.Session()
-
+    # Start time
+    startTime = time.time()
     # List of available products
     products = ["Tux", "Fox", "Python"]
 
@@ -28,7 +29,7 @@ def perform_session():
         # Print the data or error message from the query request
         try:
             response_data = response.json()
-            print(f"client no {client_no}Query result for {product}: {response_data}")
+            print(f"Query result for {product}: {response_data}")
         except json.JSONDecodeError:
             print(f"Error for {product} query: {response.text}")
 
@@ -37,7 +38,7 @@ def perform_session():
             product_data = response.json()["data"]
             if product_data.get("quantity") > 0 and random.random() < probability_order:
                 # Generate order data
-                qty=random.randint(1, product_data["quantity"])
+                qty=random.randint(1, 50)
                 order_data = {
                     "name": product,
                     "quantity": qty  # Order random quantity up to available quantity
@@ -50,13 +51,14 @@ def perform_session():
                 # Print the data or error message from the order request
                 try:
                     order_response_data = order_response.json()
-                    print(f"client no {client_no} Order result for {product}: {order_response_data}")
+                    print(f"Order result for {product}: {order_response_data}")
                 except json.JSONDecodeError:
                     print(f"Error for {product} order: {order_response.text}")
-        
-        # Sleep for a short duration before the next iteration
-        time.sleep(random.uniform(0.5, 2.0))
+    # End Time
+    endTime = time.time()
     session.close()
+    responseTime = endTime - startTime
+    print(f"***Response Time: {responseTime} seconds***")
 
 if __name__ == "__main__":
     perform_session()
